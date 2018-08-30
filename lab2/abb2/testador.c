e341b67f64e2fb96a3f18c8be9c800dbfa0d0b8f
00KL <lucasmoraesplay@gmail.com>
2018-08-29 21:28:47 -0300

    abb2 concluida

--- lab2/abb2/testador.c
+++ lab2/abb2/testador.c
@@ -1,16 +1,26 @@
 #include "abb.h"
 
+//2.  Qual  ́e a altura maxima e mınima de uma BST em funcao de N ?
+// Considerando a raiz como no 0 temos q a altura máxima seria N-1
+// A altura minima seria log n
+
+//o rand() esta ratornando repetidos impossibilitando a produção
+//de arvores muito grandes mesmo com N = 10^6
+//Media ponderada 48,40
+//A media é boa pois esta
+
 int main(){
     abb* a = criaArvore();
-    int N;
+    int N = 1000000;
 
+    printf("%d\n\n", N);
 
-    printf("Digite o numero de nós da arvore:");
-    scanf("%d", &N);
+    //printf("Digite o numero de nós da arvore:");
+    //scanf("%d", &N);
 
     a = radomAbb(a, N);
 
-    printAbb(a);
+    //printAbb(a);
     printf("\n\n");
 
     printf("%d\n", alturaAbb(a) );
