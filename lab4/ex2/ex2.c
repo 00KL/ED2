1c1c9de15a692629a89d6657387840292b1e5d68
00KL <lucasmoraesplay@gmail.com>
2018-09-13 22:11:20 -0300

    2 ta hard

--- lab4/ex2/ex2.c
+++ lab4/ex2/ex2.c
@@ -4,8 +4,36 @@
 // Escreva uma função que recebe como entrada o caminhamento em pre-order e in-order
 // de uma árvore binária e exibe em stdout o caminhamento em post-order da árvore dada.
 
-void print_post_order(int *pre, int *in, int N) {
+void print_post_order(int *pre, int *in, int last, int init) {
     // Implemente essa funcao para resolver a questao 2.
+
+    if(last != 0){
+        int i;
+        for(int j = init; j < last; j++){
+            for(i = init; i < last; i++){
+                if(pre[j] == in[i]){
+                    break;
+                }
+            }
+
+            if(pre[j] == in[i]){
+                break;
+            }
+        }
+
+
+        print_post_order(pre, in, i-1, init);
+        if(last != i+1){
+            print_post_order(pre, in, last, i+1);
+        }
+        //
+
+        printf("%d ", in[i]);
+    }
+    else{
+        printf("%d ", in[0]);
+    }
+
 }
 
 int main() {
@@ -22,7 +50,7 @@ int main() {
     }
 
     // Calcula e exibe a saida.
-    print_post_order(pre, in, N);
+    print_post_order(pre, in, N, 0);
     printf("\n");
     free(pre);
     free(in);
